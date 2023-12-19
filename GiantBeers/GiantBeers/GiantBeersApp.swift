0855b1b4ba6789ee8e057212ac3c52844d5ce0fd
ymakin <yasin.makin@icloud.com>
2023-12-19 11:25:13 +0100

    Added Xcode project

---
+++ GiantBeers/GiantBeers/GiantBeersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiantBeersApp.swift
+//  GiantBeers
+//
+//  Created by Yasin Makin on 19.12.23.
+//
+
+import SwiftUI
+
+@main
+struct GiantBeersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
