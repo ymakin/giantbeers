751c2356f5af0a52c729439aeb0f541ca7b96273
ymakin <yasin.makin@icloud.com>
2023-12-21 14:36:22 +0100

    Added beer use case for different requests

---
+++ GiantBeers/GiantBeers/usecase/BeerUseCase.swift
@@ -0,0 +1,47 @@
+//
+//  BeerUseCase.swift
+//  GiantBeers
+//
+//  Created by Yasin Makin on 19.12.23.
+//
+
+import Foundation
+
+struct BeerUseCase {
+    let network: NetworkProtocol
+    
+    var version: String {
+        "V2"
+    }
+    
+    var themePath: String {
+        "beers"
+    }
+    
+    var url: URL {
+        let baseUrl = URL(string: NetworkEnvironment.develop)!
+        let urlWithVersion = baseUrl.appendingPathComponent(version)
+        let urlWithThemePath = urlWithVersion.appendingPathComponent(themePath)
+        return urlWithThemePath
+    }
+    
+    init(network: NetworkProtocol = Network()) {
+        self.network = network
+    }
+    
+    func detailBeerRequest(_with id: String) -> ResultType<Beer> {
+        let urlWithId = url.appendingPathComponent(id)
+        
+        Task {
+            let result: ResultType<Beer> = await network.request(url: urlWithId)
+            return result
+        }
+        
+        return .error(type: .undefined)
+    }
+    
+    func beersRequest(_with requestParameter: RequestParameter) async -> ResultType<[Beer]> {
+        let result: ResultType<[Beer]> = await network.request(url: url)
+        return result
+    }
+}
