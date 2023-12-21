7c5ae2c294486fc5e15de75b0673e196f339f2ca
ymakin <yasin.makin@icloud.com>
2023-12-21 14:25:02 +0100

    Adjusted Quantity model

--- GiantBeers/GiantBeers/model/Quantity.swift
+++ GiantBeers/GiantBeers/model/Quantity.swift
@@ -8,12 +8,6 @@
 import Foundation
 
 struct Quantity: Decodable, Hashable {
-    enum UnitType: Decodable, Hashable {
-        case litres
-        case celsius
-        case kilograms
-    }
-    
     let value: Float
-    let unit: UnitType
+    let unit: String
 }
